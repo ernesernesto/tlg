340cf1c8431f79a86d9f64be22338e83bed7f815
ernesernesto <ernesernesto@gmail.com>
2024-05-13 10:13:14 +0700

    update video and add rainbow effect

--- q6/player_onMove.lua
+++ q6/player_onMove.lua
@@ -1,5 +1,7 @@
 local ec = EventCallback
 
+local outfit = nil
+
 ec.onMove = function(self, position, direction)
 	position:getNextPosition(direction)
 
@@ -10,6 +12,26 @@ ec.onMove = function(self, position, direction)
 		position:getNextPosition(direction)
 		toTile = Tile(position)
 	end
+
+	outfit = self:getOutfit()
+	rainbowEffect(self:getId(), outfit.lookType, 0)
 end
 
 ec:register()
+
+function rainbowEffect(cid, lookType, index)
+	local creature = Creature(cid)
+	if index < 3 then
+		local colors = { 94, 77, 79, 82, 87, 90 }
+		creature:setOutfit({
+			lookType = lookType,
+			lookHead = colors[((index) % 6) + 1],
+			lookBody = colors[((index + 1) % 6) + 1],
+			lookLegs = colors[((index + 2) % 6) + 1],
+			lookFeet = colors[((index + 3) % 6) + 1]
+		})
+		addEvent(rainbowEffect, 50, cid, lookType, index + 1)
+	else
+		creature:setOutfit(outfit)
+	end
+end
